e7935868b22db4ea76f2fdc5efa09894a7b41bc0
grigkatrin <grig.katrin@gmail.com>
2020-09-11 18:34:00 +0300

    lab_1 finished

---
+++ students/K33401/Grigoreva Ekaterina/Lab1/task_1/client.py
@@ -0,0 +1,11 @@
+import socket
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect(('localhost', 8080))
+sock.sendall(b'Hello, server!')
+
+data = sock.recv(1024)
+print(data.decode("utf-8"))
+
+sock.close()
