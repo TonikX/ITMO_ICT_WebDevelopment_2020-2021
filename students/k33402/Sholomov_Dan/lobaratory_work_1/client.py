638ab5ad7debf379b55ec1fdc7738f7c2b906139
heidamn <heidamn@gmail.com>
2020-09-17 22:26:01 +0000

    first lab second task finished

--- students/k33402/Sholomov_Dan/lobaratory_work_1/client.py
+++ students/k33402/Sholomov_Dan/lobaratory_work_1/client.py
@@ -5,13 +5,20 @@ def task1():
     sock = socket.socket()
     sock.connect(('localhost', 11111))
     sock.send(bytes('Hello, server', "utf-8"))
-
     data = sock.recv(1024)
     sock.close()
-
     print(data.decode('utf-8'))
 
 
-# Press the green button in the gutter to run the script.
+def task2():
+    sock = socket.socket()
+    sock.connect(('localhost', 22222))
+    print("Введите высоту и сторону параллелограмма через пробел:")
+    sock.send(bytes(input(), "utf-8"))
+    data = sock.recv(1024)
+    sock.close()
+    print(data.decode('utf-8'))
+
+
 if __name__ == '__main__':
-    task1()
+    task2()
--- students/k33402/Sholomov_Dan/lobaratory_work_1/server.py
+++ students/k33402/Sholomov_Dan/lobaratory_work_1/server.py
@@ -15,5 +15,22 @@ def task1():
     conn.close()
 
 
+def task2():
+    sock = socket.socket()
+    sock.bind(('', 22222))
+    sock.listen(1)
+    conn, _ = sock.accept()
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        inp = data.decode('utf-8').split()
+        if len(inp) != 2:
+            conn.send(bytes('Неверный формат ввода', "utf-8"))
+            break
+        conn.send(bytes(str(int(inp[0])*int(inp[1])), "utf-8"))
+    conn.close()
+
+
 if __name__ == '__main__':
-    task1()
+    task2()
