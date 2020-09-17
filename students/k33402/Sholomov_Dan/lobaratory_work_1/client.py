25296c9e1c531f0f67100722562077838c0352a7
heidamn <heidamn@gmail.com>
2020-09-17 22:08:33 +0000

    first lab first task finished

---
+++ students/k33402/Sholomov_Dan/lobaratory_work_1/client.py
@@ -0,0 +1,17 @@
+import socket
+
+
+def task1():
+    sock = socket.socket()
+    sock.connect(('localhost', 11111))
+    sock.send(bytes('Hello, server', "utf-8"))
+
+    data = sock.recv(1024)
+    sock.close()
+
+    print(data.decode('utf-8'))
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    task1()
