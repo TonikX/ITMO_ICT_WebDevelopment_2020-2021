a4df09fbb87025c21366429e7bac1a1c64b43ce7
FataMorganaAG <arina.griz@mail.ru>
2020-09-25 00:51:55 +0300

    Лаборатоная №1 Грицай Арина

---
+++ students/K33421/Грицай Арина/laboratory_work_1/Task_1/t1_client.py
@@ -0,0 +1,13 @@
+import socket
+
+conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+conn.connect(("127.0.0.1", 14900))
+
+client_message = "Hello, server!"
+conn.send(bytes(client_message, "utf-8"))
+data_recv = conn.recv(1024)
+data_recv = data_recv.decode('utf-8')
+print(("Data recieved: " + data_recv))
+
+conn.close()
+
