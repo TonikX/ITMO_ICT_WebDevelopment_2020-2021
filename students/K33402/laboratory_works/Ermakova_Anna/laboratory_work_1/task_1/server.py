8e3247f4d5a13d530f5dfa9bcdcf20b4750721aa
ShadowErm <shadow0touch@gmail.com>
2020-09-25 23:32:06 +0300

    Добавлена первая лаба

---
+++ students/K33402/laboratory_works/Ermakova_Anna/laboratory_work_1/task_1/server.py
@@ -0,0 +1,14 @@
+import socket
+
+sock = socket.socket()
+sock.bind(('', 9090))
+sock.listen(1)
+conn, addr = sock.accept()
+print ('connected:', addr)
+while True:
+    data = conn.recv(1024)
+    if not data:
+        break
+    print(data.decode("utf-8"))
+    conn.send(b'Hello, client!')
+conn.close()
---
+++ students/K33402/laboratory_works/Ermakova_Anna/laboratory_work_1/task_3/client.py
@@ -0,0 +1,10 @@
+import socket
+
+sock = socket.socket()
+sock.connect(('localhost', 9090))
+while True:
+    data = sock.recv(1024)
+    if not data:
+        break
+    print(data.decode('utf-8'))
+sock.close()
