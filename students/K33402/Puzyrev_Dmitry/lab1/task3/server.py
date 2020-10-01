60257d7852d72411bddfa365d1dced7b59caa6d8
Dmitry Puzyrev <puzyrev.dmitry@gmail.com>
2020-10-01 11:18:03 +0300

    Update task3 and report

--- students/K33402/Puzyrev_Dmitry/lab1/task3/server.py
+++ students/K33402/Puzyrev_Dmitry/lab1/task3/server.py
@@ -1,24 +1,31 @@
 import socket
 
-server = socket.socket()
-host = "127.0.0.1"
-port = 8000
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = "localhost"
+port = 8880
 server.bind((host, port))
 
 print("Starting server on", host, port)
 print("The web server URL for this would be http://%s:%d/" % (host, port))
 
-server.listen(5)
+server.listen(1)
 
 print("Entering...")
+
 while True:
 	client, (client_host, client_port) = server.accept()
 	print("Got connection from", client_host, client_port)
 
-	response_type = "HTTP/1.0 200 OK\n"
+	response_type = "HTTP/1.1 200 OK\n"
 	headers = "Content-Type: text/html\n\n"
-	body = open("index.html", "r").read()
 
+	body = open("index.html", "r").read()
 	response = response_type + headers + body
-	client.send(response.encode())
-	client.close()
+
+	client.sendall(response.encode())
+
+	data = client.recv(1024)
+	if not data:
+		break
+
+client.close()
