3f2aee49cd5d203316c4d4e27d76e48fdaba05cb
Anna Malinina <anna-maks99@lenta.ru>
2020-10-01 11:13:09 +0300

    fix task 3

--- students/K33402/laboratory_works/Malinina_Anna/laboratory_work_1/task_3/server.py
+++ students/K33402/laboratory_works/Malinina_Anna/laboratory_work_1/task_3/server.py
@@ -4,6 +4,10 @@ if __name__ == '__main__':
     sock = socket.socket()
     sock.bind(('', 9090))
     sock.listen(1)
-    conn, addr = sock.accept()
-    conn.sendall(b'HTTP/1.0 200 OK\nContent-Type: text/html\n\n' + open('index.html', 'rb').read())
-    conn.close()
+    while True:
+        conn, addr = sock.accept()
+        data = conn.recv(1024)
+        if not data:
+            break
+        conn.sendall(b'HTTP/1.0 200 OK\r\nContent-Type: text/html\r\n\r\n' + open('index.html', 'rb').read())
+        conn.close()
