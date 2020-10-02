e15f3cbd82cef62ef255f887ad93bca161b3c42d
spiritofsofya <chanovas@outlook.com>
2020-10-02 13:34:03 +0500

    task_3 fixed

--- students/K33422/laboratory_works/Chanova_Sofya/laboratory_work_1/task_3/task_3_client.py
+++ students/K33422/laboratory_works/Chanova_Sofya/laboratory_work_1/task_3/task_3_client.py
@@ -2,14 +2,11 @@ import socket
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = 'localhost'
-port = 14042
+port = 14006
 client_socket.connect((host, port))
 
-while True:
-    data = client_socket.recv(1024)
-    if not data:
-        break
-    print(data.decode('utf-8'))
+data = client_socket.recv(1024)
+
+print(data.decode('utf-8'))
 
 client_socket.close()
-
--- students/K33422/laboratory_works/Chanova_Sofya/laboratory_work_1/task_3/task_3_server.py
+++ students/K33422/laboratory_works/Chanova_Sofya/laboratory_work_1/task_3/task_3_server.py
@@ -2,25 +2,23 @@ import socket
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = 'localhost'
-port = 14042
+port = 14006
 server_socket.bind((host, port))
-
+server_socket.listen(1)
 
 print('starting server on: ', host, port, ' press ctrl+c to exit')
 
-server_socket.listen(1)
-client_socket, client_address = server_socket.accept()
-
 while True:
-    with open('index.html', 'r') as page:
-        response_type = 'HTTP/1.0 200 OK\n'
-        headers = 'Connect-Type: text/html\n\n'
-        body = ''.join(page)
-        response = response_type + headers + body
-        client_socket.send(response.encode('utf-8'))
-    data = client_socket.recv(1024)
-    if not data:
+    try:
+        client_socket, client_address = server_socket.accept()
+        with open('index.html', 'r') as page:
+            response_type = 'HTTP/1.0 200 OK\n'
+            headers = 'Connect-Type: text/html\n\n'
+            body = ''.join(page)
+            response = response_type + headers + body
+            client_socket.send(response.encode('utf-8'))
+            client_socket.close()
+    except KeyboardInterrupt:
         break
 
-client_socket.close()
-
+server_socket.close()
