3f3dadce0d731fb8bfabb102f22bfec8b8746460
SpaceOcean <komsomolenko11@gmail.com>
2020-10-06 23:56:22 +0300

    add p.w. 2.2

--- students/K33401/practical_works/Komsomolenko_Vlad/simple_django_web_project/django_project_komsomolenko/project_first_app/urls.py
+++ students/K33401/practical_works/Komsomolenko_Vlad/simple_django_web_project/django_project_komsomolenko/project_first_app/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 urlpatterns = [
     path('owners/', views.owner, name='owner'),
-    path('owners/<int:pk>/', views.owner_id.as_view()),
+    path('owner/<int:pk>/', views.owner_id.as_view()),
+    path('cars/', views.car.as_view()),
+    path('car/<int:pk>/', views.car_id.as_view()),
+    path('car/update/<int:pk>/', views.car_Update.as_view()),
+    path('create_owner/', views.owner_create),
+    path('car/delete/<int:pk>/', views.car_delete.as_view()),
+    path('cars/create/', views.car_add.as_view()),
 ]
