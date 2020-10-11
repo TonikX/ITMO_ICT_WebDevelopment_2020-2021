98c79edc2e28d868a7648e52ab08fdf840f6bd68
spi__sladko <inessaivanovastr@inbox.ru>
2020-10-11 12:45:00 +0300

    Практика 1.1 и 1.2

---
+++ students/K33402/Ivanova_Inessa/simple_django_web_project/project_first_app/models.py
@@ -0,0 +1,35 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+from datetime import date
+
+class Person(models.Model):
+    name = models.TextField()
+    surname = models.TextField()
+    birthday = models.DateField()
+
+class Car(models.Model):
+    brand = models.TextField()
+    model = models.TextField()
+    color = models.TextField()
+    number = models.CharField(max_length=8)
+    owner = models.ManyToManyField(Person, through='Ownership', null=True)
+
+class DriverLicence(models.Model):
+    CATEGORIES = (
+        ('A', 'Moto'),
+        ('B', 'Car'),
+        ('C', 'Truck'),
+        ('D', 'Bus'),
+    )
+    number = models.PositiveIntegerField()
+    date = models.DateField()
+    category = models.CharField(max_length=1, choices=CATEGORIES)
+    driver = models.ForeignKey(Person, on_delete=models.CASCADE, null=True)
+    
+class Ownership(models.Model):
+    start = models.DateField()
+    end = models.DateField()
+    car = models.ForeignKey(Car, on_delete=models.CASCADE, null=True)
+    driver = models.ForeignKey(Person, on_delete=models.CASCADE, null=True)
+    
