fb15d6c099b9aeaaea88343aed326f5a43e1b2d4
Jabkevich <dmitry.jabkevich@gmail.com>
2020-10-14 19:35:44 +0300

    2 practic

--- students/K33402/lr2/django_project_Li/project_first_app/admin.py
+++ students/K33402/lr2/django_project_Li/project_first_app/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Driver, DriverDocument, Possession, Car
-# Register your models here.
 
+# Register your models here.
 
 
 admin.site.register(Driver)
--- students/K33402/lr2/django_project_Li/project_first_app/urls.py
+++ students/K33402/lr2/django_project_Li/project_first_app/urls.py
@@ -2,7 +2,15 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('Drivers/', views.driverF)
+    path('Drivers/', views.driverF, name='Drivers'),
+    path('Car/', views.CarF, name="Car"),
+   # path('addCar/', views.addCar),
+    path('create_car', views.CarCreate.as_view(success_url = '/Car/'), name = 'create_car'),
+    path('car/<int:pk>/delete/', views.CarDeleteView.as_view(success_url = '/Car/')),
+    path('car/<int:pk>/update/', views.CarUpdateView.as_view(success_url = '/Car/')),
+    path('Possession/', views.PossessionF),
+    path('DriverDocument/', views.DriverDocumentF),
+    path('addDrivers/', views.addDrivers)
 ]
