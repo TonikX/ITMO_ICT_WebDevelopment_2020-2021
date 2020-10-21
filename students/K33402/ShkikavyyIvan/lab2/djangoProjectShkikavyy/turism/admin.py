8aa9c54dcf2b965b156ca7a66e0ffb7a5df8d252
bigcoachpotato <48162523+bigcoachpotato@users.noreply.github.com>
2020-10-21 16:07:34 +0300

    deleting

--- students/K33402/ShkikavyyIvan/lab2/djangoProjectShkikavyy/turism/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import User, Tour, Comment, Reservation
-from django.contrib.auth.admin import UserAdmin
-
-admin.site.register(User, UserAdmin)
-admin.site.register(Tour)
-admin.site.register(Comment)
-admin.site.register(Reservation)
