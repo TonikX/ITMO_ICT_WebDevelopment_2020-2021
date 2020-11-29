54ba492860fa3bd90a8617df4ec26e6f7245ddad
themrdoc111 <330580art330580>
2020-11-29 18:19:14 +0300

    add pw3.1

---
+++ students/K33401/practical_works/Тарасов_Артём/simple_drf_project/warriors/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render
+from rest_framework.views import APIView, Response
+from rest_framework import generics
+from django.shortcuts import get_object_or_404
+
+from .models import *
+from .serializers import *
+
+
+class WarriorAPIView(APIView):
+  def get(self, request):
+    skills = Skill.objects.all()
+    serializer = SkillRelatedSerializer(skills, many=True)
+    return Response({'Skills': serializer.data})
+
+  def post(self, request):
+    serializer = WarriorSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+      article_saved = serializer.save()
+      return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+  def put(self, request, pk=None):
+    warrior = get_object_or_404(Warrior, pk=pk)
+    serializer = WarriorSerializer(warrior, data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+  def delete(self, request, pk):
+    warrior = get_object_or_404(Warrior, pk=pk)
+    try:
+      warrior.delete()
+      return Response(status=200)
+    except:
+      return Response(status=400)
+
+class WarriorCreateAPIView(generics.ListCreateAPIView):
+  serializer_class = WarriorSerializer
+  queryset = Warrior.objects.all()
+
+class WarriorDestroyView(generics.DestroyAPIView):
+  queryset = Warrior.objects.all()
+  serializer_class = WarriorSerializer
+
+class WarriorUpdateView(generics.UpdateAPIView):
+  queryset = Warrior.objects.all()
+  serializer_class = WarriorSerializer
+
