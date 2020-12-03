92317ec48e468ecc387ead8650afc03370cf5375
Vladimir <vova.goncharov.2001@gmail.com>
2020-12-03 05:28:23 +0300

    ЛР3 Добавлена авторизация и модель БД

---
+++ students/K33401/Goncharov_Vladimir/Lr3/hotel/hotel/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include, re_path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('auth/', include('djoser.urls')),
+    re_path(r'^auth/', include('djoser.urls.authtoken')),
+    #path('hotel/', include('hotel_app.urls')),
+]
