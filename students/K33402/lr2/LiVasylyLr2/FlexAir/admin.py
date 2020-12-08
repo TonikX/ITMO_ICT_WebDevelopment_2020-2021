0a916467de577626d7a7d1b19a858e8dc45d76d9
Замятин Дмитрий <dmitry.jabkevich@gmail.com>
2020-12-08 15:54:48 +0300

    2 lr done

---
+++ students/K33402/lr2/LiVasylyLr2/FlexAir/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+admin.site.register(Flight)
+admin.site.register(Airlines)
+admin.site.register(Place)
+admin.site.register(Comments)
+
---
+++ students/K33402/lr2/LiVasylyLr2/FlexAir/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', HomeListView.as_view(), name='home'),
+    path('detailFuture/<int:pk>', HomeDetailViewFuture.as_view(), name='detailFuture_page'),
+    path('lastflight', LastFlightView.as_view(), name='flight_page'),
+    path('detailPast/<int:pk>', HomeDetailViewPast.as_view(), name='detailPast_page'),
+    path('login', MyprojectLoginView.as_view(), name='login_page'),
+    path('logout', MyprojectLogOutView.as_view(), name='logout_page'),
+    path('user', UserPage.as_view(), name='user_page'),
+    path('regiser', RegisterUserView.as_view(), name='register_page'),
+]
+
