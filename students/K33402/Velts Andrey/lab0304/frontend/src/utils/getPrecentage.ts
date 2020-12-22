35fff8051660cc406a75d8f69e63e84f010208de
ThreshHNS <herovelts1@gmail.com>
2020-12-22 21:04:51 +0800

    Добавили tsdoc и обновили Mkdocs

--- students/K33402/Velts Andrey/lab0304/frontend/src/utils/getPrecentage.ts
+++ students/K33402/Velts Andrey/lab0304/frontend/src/utils/getPrecentage.ts
@@ -1,3 +1,11 @@
+/**
+ * Returns the precentage of two numbers.
+ *
+ * @param partialValue- The part of the overall value
+ * @param totalValue - The overall value
+ * @returns The percentage of partialValue from totalValue
+ *
+ */
 export default function getPrecentage(
   partialValue: number,
   totalValue: number
--- students/K33402/Velts Andrey/lab0304/frontend/src/utils/parseUnix.ts
+++ students/K33402/Velts Andrey/lab0304/frontend/src/utils/parseUnix.ts
@@ -1,5 +1,12 @@
 import { fromUnixTime } from "date-fns";
 
+/**
+ * Parsing a unix timestamp to date.
+ *
+ * @param date - Date in unix timestamp
+ * @returns The Date object
+ *
+ */
 const parseUnix = (date: string): Date => {
   return fromUnixTime(parseInt(date));
 };
