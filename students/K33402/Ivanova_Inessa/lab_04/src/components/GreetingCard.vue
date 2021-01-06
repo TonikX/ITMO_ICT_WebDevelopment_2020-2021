2711fbbca4ad4d0519083f90fb96c036ce9ed225
spi__sladko <inessaivanovastr@inbox.ru>
2021-01-06 13:07:21 +0300

    .

--- students/K33402/Ivanova_Inessa/lab_04/src/components/GreetingCard.vue
+++ students/K33402/Ivanova_Inessa/lab_04/src/components/GreetingCard.vue
@@ -1,12 +1,23 @@
+
 <template>
-  <h1>Привет!</h1>
+  <section>
+  <h1>Привет<span :username="username" v-if="username">, {{ username }}</span>!</h1>
+  </section>
 </template>
 
 <script>
 export default {
-  name: 'GreetingCard'
+  name: 'GreetingCard',
+
+  props: {
+    username: String,
+    savedUsername: String
+  }
 }
 </script>
 
 <style>
+.greeting-list li {
+  text-align: left;
+}
 </style>
