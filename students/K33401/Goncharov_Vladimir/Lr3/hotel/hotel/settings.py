9a2cab8cb4035ded826ba1dd0df6d0b012f49a9b
Vladimir <vova.goncharov.2001@gmail.com>
2021-01-18 01:29:45 +0300

    Добавлена фронтенд часть + cors джанги

--- students/K33401/Goncharov_Vladimir/Lr3/hotel/hotel/settings.py
+++ students/K33401/Goncharov_Vladimir/Lr3/hotel/hotel/settings.py
@@ -41,10 +41,12 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'hotel_app',
     'drf_yasg',
-    'djoser'
+    'djoser',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -129,3 +131,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = True
