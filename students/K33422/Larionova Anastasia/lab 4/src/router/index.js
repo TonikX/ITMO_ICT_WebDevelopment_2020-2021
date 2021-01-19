46e30f326fa57dc24e5f58e3e7cd27d5fa882ff0
Anastasia Larionova <lanastya@gmail.com>
2021-01-19 23:20:55 +0300

    Added lab 4

---
+++ students/K33422/Larionova Anastasia/lab 4/src/router/index.js
@@ -0,0 +1,57 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import SignUp from '../views/SignUp.vue'
+import SignIn from '../views/SignIn.vue'
+import UserInfo from '../views/Settings.vue'
+import ChickensList from '../views/Chickens.vue'
+import BreedsList from '../views/Breeds.vue'
+import CellsList from '../views/Cells.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/signup',
+    name: 'SignUp',
+    component: SignUp
+  },
+  {
+    path: '/signin',
+    name: 'SignIn',
+    component: SignIn
+  },
+  {
+    path: '/settings',
+    name: 'UserInfo',
+    component: UserInfo
+  },
+  {
+    path: '/chickenslist',
+    name: 'ChickensList',
+    component: ChickensList
+  },
+  {
+    path: '/breedslist',
+    name: 'BreedsList',
+    component: BreedsList
+  },
+  {
+    path: '/cellslist',
+    name: 'CellsList',
+    component: CellsList
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
