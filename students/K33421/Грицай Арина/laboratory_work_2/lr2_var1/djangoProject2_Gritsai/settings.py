4215fd8b2d83019fcbf4990f5ec7c8dddf6df57a
FataMorganaAG <arina.griz@mail.ru>
2021-01-19 19:19:30 +0300

    Gritsai Arina lab, pract

--- students/K33421/Грицай Арина/laboratory_work_2/lr2_var1/djangoProject2_Gritsai/settings.py
+++ students/K33421/Грицай Арина/laboratory_work_2/lr2_var1/djangoProject2_Gritsai/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'djangoProject2_Gritsai.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+         'NAME': 'hotels',
+         'USER': 'admin',
+         'PASSWORD': 'admin1',
+         'HOST': '127.0.0.1',
+         'PORT': '8000',
     }
 }
 
