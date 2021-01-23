3a3a75dd8fc5585db2283737a49c331a27025824
Dante Leapman <danteleapman@mail.ru>
2021-01-23 21:32:03 +0300

    changed some files

--- students/K33422/Khusnutdinov_Sergei/Lab_03/birds/birds/settings.py
+++ students/K33422/Khusnutdinov_Sergei/Lab_03/birds/birds/settings.py
@@ -107,6 +107,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+}
+
 AUTH_USER_MODEL = 'birds_app.User'
 
 # Internationalization
