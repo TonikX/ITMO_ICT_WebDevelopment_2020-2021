6c276d0dff4388cfe040aa58594031c50f4de701
Andrey <iHenk0@yandex.ru>
2021-01-24 16:37:50 +0300

    Practical 3.2 Done

--- students/K33402/Beresnev_Andrey/practical3.1/warriors_project/warriors_app/views.py
+++ students/K33402/Beresnev_Andrey/practical3.1/warriors_project/warriors_app/views.py
@@ -1,8 +1,39 @@
-from django.shortcuts import render
+from rest_framework import generics, viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import Warrior, Skill, Profession, SkillOfWarrior
+from .serializers import WarriorSerializer, SkillSerializer, ProfessionSerializer, WarriorPartialSerializer, SkillOfWarriorSerializer
 
 
-class WarriorAPIView(APIView):
+class WarriorAPIView(viewsets.ModelViewSet):
+    queryset = Warrior.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == 'list' or self.action == 'retrieve':
+            return WarriorSerializer
+        return WarriorPartialSerializer
+
+
+class SkillAPIView(APIView):
     def get(self, request):
-        warriors = Warrior.objects.all()
-        serializer = WarriorSerializer(warriors, many=True)
-        return Response({"Warriors": serializer.data})
+        skills = Skill.objects.all()
+        serializer = SkillSerializer(skills, many=True)
+        return Response({"Skills": serializer.data})
+
+    def post(self, request):
+        serializer = SkillSerializer(data=request.data)
+
+        if serializer.is_valid(raise_exception=True):
+            skill_saved = serializer.save()
+            return Response({"Success": "skill '{}' created successfully.".format(skill_saved.title)})
+
+
+class ProfessionCreateAPIView(generics.ListCreateAPIView):
+    serializer_class = ProfessionSerializer
+    queryset = Profession.objects.all()
+
+
+class SkillOfWarriorAPIView(viewsets.ModelViewSet):
+    serializer_class = SkillOfWarriorSerializer
+    queryset = SkillOfWarrior.objects.all()
