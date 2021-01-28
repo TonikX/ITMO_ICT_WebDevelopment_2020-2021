ae8a8d162436790ecf8c561d9a0c534e07cb221d
Andrey <iHenk0@yandex.ru>
2021-01-28 17:09:37 +0300

    Practical 4 + Login Lab4

---
+++ students/K33402/Beresnev_Andrey/lab4/hotel_frontend/src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Login from "@/views/Login.vue";
+import Register from "@/views/Register.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "/signup",
+    name: "Register",
+    component: Register,
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
