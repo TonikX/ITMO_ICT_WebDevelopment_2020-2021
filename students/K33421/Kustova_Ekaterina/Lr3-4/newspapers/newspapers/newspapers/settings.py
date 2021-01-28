4a69d53f3d8751590fcb08e3c99434e63f4524de
IJustWantToSleep <Katya.Kustova@gmail.com>
2021-01-29 00:47:19 +0300

    Добавлена 4 лабораторная

--- students/K33421/Kustova_Ekaterina/Lr3-4/newspapers/newspapers/newspapers/settings.py
+++ students/K33421/Kustova_Ekaterina/Lr3-4/newspapers/newspapers/newspapers/settings.py
@@ -41,10 +41,14 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'djoser',
     'newspapers_distribution',
+    'corsheaders'
 
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.BrokenLinkEmailsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -53,6 +57,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'newspapers.urls'
 
