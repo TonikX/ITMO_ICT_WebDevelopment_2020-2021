47eb0cdf7c7dbe8d071bc4fd3f1ac94848475e7b
heidamn <heidamn@gmail.com>
2021-01-31 21:38:27 +0300

    lab4 + practical works

---
+++ students/k33402/Sholomov_Dan/warriors_project/warriors_app/serializers.py
@@ -0,0 +1,84 @@
+from rest_framework import serializers
+
+from warriors_app.models import Warrior, Skill, Profession, SkillOfWarrior
+
+
+class WarriorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Warrior
+        fields = "__all__"
+
+
+class SkillSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Skill
+        fields = ["title"]
+
+
+class SkillOfWarriorSerializer(serializers.ModelSerializer):
+    warrior_skills = WarriorSerializer(many=True)
+
+    class Meta:
+        model = Skill
+        fields = ['title', 'warrior_skills']
+
+
+class ProfessionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profession
+        fields = ["title", "description"]
+
+
+class ProfessionCreateSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=120)
+    description = serializers.CharField()
+
+    def create(self, validated_data):
+        profession = Profession(**validated_data)
+        profession.save()
+        return Profession(**validated_data)
+
+
+class WarriorNestedSerializer(serializers.ModelSerializer):
+    profession = ProfessionSerializer()
+    skill = SkillSerializer(many=True)
+
+    race = serializers.CharField(source="get_race_display", read_only=True)
+
+    class Meta:
+        model = Warrior
+        fields = "__all__"
+
+    def create(self, validated_data):
+        profession = Profession.objects.create(title=validated_data['profession']['title'],
+                                               description=validated_data['profession']['description'])
+        profession.save()
+        instance = Warrior.objects.create(name=validated_data['name'], level=validated_data['level'],
+                                          profession=profession)
+        for skill in validated_data['skill']:
+            skill_obj = Skill.objects.create(**skill)
+            skill_obj.save()
+            skill_of_warrior = SkillOfWarrior.objects.create(skill=skill_obj, warrior=instance)
+            skill_of_warrior.save()
+
+        return instance
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data['name']
+        instance.level = validated_data['level']
+        instance.profession.title = validated_data['profession']['title']
+        instance.profession.description = validated_data['profession']['description']
+        instance.save()
+        skills = list(instance.skill.all())
+
+        for input_skill in validated_data['skill']:
+            skill = skills.pop(0)
+            skill.title = input_skill['title']
+            skill.save()
+
+        return instance
+
+    def to_representation(self, instance):
+        representation = super(WarriorNestedSerializer, self).to_representation(instance)
+        representation['skill'] = SkillOfWarriorSerializer(instance.skill.all(), many=True).data
+        return representation
