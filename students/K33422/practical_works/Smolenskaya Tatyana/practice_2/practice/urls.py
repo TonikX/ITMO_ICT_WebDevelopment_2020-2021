5f7ea98dc8e239a2cb11d96c0d651700969589a6
Tanya <smolenskya0089@mail.ru>
2021-03-08 22:13:18 +0300

    Добавлена практическая работа для второй лабораторной работы

---
+++ students/K33422/practical_works/Smolenskaya Tatyana/practice_2/practice/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('driver/', get_drivers),
+    path('driver/<int:driver_id>/', get_driver),
+    path('drivers/', get_drivers),
+    path('cars/', CarsListView.as_view()),
+    path('car/<int:pk>/', CarDetailView.as_view()),
+    path('car/<int:pk>/update/', CarUpdateView.as_view()),
+    path('driver/create/', create_view),
+    path('car/create/', CarCreate.as_view()),
+    path('car/<int:pk>/delete/', CarDeleteView.as_view()),
+]
