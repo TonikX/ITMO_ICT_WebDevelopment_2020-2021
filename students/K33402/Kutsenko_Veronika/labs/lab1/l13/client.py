922595675a4415556ff5a46873d06e6202db0a4d
vktsn <44133566+vktsn@users.noreply.github.com>
2021-03-12 16:39:05 +0700

    Delete students/K33402/Kutsenko_Veronika/labs directory

--- students/K33402/Kutsenko_Veronika/labs/lab1/l13/client.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-s = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 9090))
-
-s.sendall(bytes(f'.', 'utf-8'))
-data = s.recv(1024)
-print(data.decode('utf-8'))
-s.close()
