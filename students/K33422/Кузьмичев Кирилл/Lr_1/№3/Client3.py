c6612abc47b56647df9c954ede94c85bf3b7091a
kirmrik <44197203+kirmrik@users.noreply.github.com>
2021-03-13 18:35:10 +0300

    Исправлена 1 лабораторная работа

--- students/K33422/Кузьмичев Кирилл/Lr_1/№3/Client3.py
+++ students/K33422/Кузьмичев Кирилл/Lr_1/№3/Client3.py
@@ -1,7 +1,7 @@
 import socket
 
 Sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-Sock.connect(("127.0.0.1", 11042))
+Sock.connect(("127.0.0.3", 11042))
 Sock.send('Hello, server!'.encode("utf-8"))
 
 data = Sock.recv(1024)
--- students/K33422/Кузьмичев Кирилл/Lr_1/№3/Server3.py
+++ students/K33422/Кузьмичев Кирилл/Lr_1/№3/Server3.py
@@ -1,16 +1,23 @@
 import socket
+import webbrowser
 
 Sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-Sock.bind(("127.0.0.1", 11042))
+Sock.bind(("127.0.0.3", 11042))
 Sock.listen(1)
 conn, addr = Sock.accept()
 
+print('connected: ', addr)
+
 while True:
-    with open('index.html', 'r') as file:
-        browse = file.read()
-        conn.sendall(bytes(f'{browse}', 'utf-8'))
     data = conn.recv(1024)
     if not data:
         break
+    with open('index.html', 'rb') as file:
+        html = file.read()
+        conn.sendall(bytes(f'HTTP/1.1 200 OK\nContent-Type: text/html\n\n{html}', 'utf-8'))
+        firefox_path = "C:\Program Files\Mozilla Firefox\firefox.exe"
+        webbrowser.register('firefox', None, (webbrowser.BackgroundBrowser(firefox_path), 1))
+        webbrowser.get(using ='firefox')
+        webbrowser.open('index.html', new=2)
 
 conn.close()
