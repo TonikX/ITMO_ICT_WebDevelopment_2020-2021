f6f7b0fdea2ae6302037e88058629bb35de22254
Panda0412 <nasb_99@mail.ru>
2021-04-06 16:21:55 +0300

    practice 2.1

--- students/K33402/practical_works/Bugaeva_Anastasiia/simple_django_web_project/django_project_Bugaeva/project_first_app/models.py
+++ students/K33402/practical_works/Bugaeva_Anastasiia/simple_django_web_project/django_project_Bugaeva/project_first_app/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+
+class Car(models.Model):
+    state_number = models.CharField(max_length=15)
+    brand = models.CharField(max_length=20)
+    model = models.CharField(max_length=20)
+    color = models.CharField(max_length=30)
+
+    def __str__(self):
+        return "{} {}".format(self.brand, self.model)
+
+
+class CarOwner(models.Model):
+    surname = models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
+    birthday = models.DateField()
+    cars = models.ManyToManyField(Car, through='Ownership')
+
+    def __str__(self):
+        return "{} {}".format(self.name, self.surname)
+
+
+class Ownership(models.Model):
+    owner = models.ForeignKey(CarOwner, on_delete=models.SET_NULL, null=True)
+    car = models.ForeignKey(Car, on_delete=models.SET_NULL, null=True)
+    date_start = models.DateField()
+    date_end = models.DateField(null=True)
+
+
+class DriverLicense(models.Model):
+    CATEGORIES = (
+        ('A', 'Мотоциклы'),
+        ('B', 'Легковые автомобили'),
+        ('C', 'Грузовые автомобили'),
+        ('D', 'Пассажирские автомобили'),
+        ('M', 'Мопеды и легкие квадроциклы'),
+        ('Tm', 'Трамваи'),
+        ('Tb', 'Троллейбусы'),
+    )
+
+    driver = models.ForeignKey(CarOwner, on_delete=models.CASCADE)
+    id_number = models.IntegerField()
+    type = models.CharField(max_length=2, choices=CATEGORIES)
+    date = models.DateField()
+
--- students/K33402/practical_works/Bugaeva_Anastasiia/simple_django_web_project/django_project_Bugaeva/project_first_app/views.py
+++ students/K33402/practical_works/Bugaeva_Anastasiia/simple_django_web_project/django_project_Bugaeva/project_first_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import Http404
+from .models import CarOwner
 
-# Create your views here.
+
+def get_owner(request, owner_id):
+    try:
+        car_owner = CarOwner.objects.get(pk=owner_id)
+    except CarOwner.DoesNotExist:
+        raise Http404('Owner doesn\'t exist')
+    return render(request, 'owner.html', {'owner': car_owner})
