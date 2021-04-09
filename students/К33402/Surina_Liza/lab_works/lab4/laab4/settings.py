54624d2decced65c23745fdaebf69f76592e7e4e
lololol0 <elzsurn@gmail.com>
2021-04-09 17:36:07 +0300

    добавлена документация

--- students/К33402/Surina_Liza/lab_works/lab4/laab4/settings.py
+++ students/К33402/Surina_Liza/lab_works/lab4/laab4/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'corsheaders',
     'djoser',
+    'drf_yasg',
 
     'movie',
 ]
--- students/К33402/Surina_Liza/lab_works/lab4/laab4/urls.py
+++ students/К33402/Surina_Liza/lab_works/lab4/laab4/urls.py
@@ -17,12 +17,31 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+schema_view = get_schema_view(
+   openapi.Info(
+      title="API",
+      default_version='v2',
+      description="Description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="hardbeat34@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('djoser.urls')),
     path('api/v1/', include('djoser.urls.authtoken')),
-    path('api/v1/', include('movie.urls'))
+    path('api/v1/', include('movie.urls')),
+    path('doc/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('doc/redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
+
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
